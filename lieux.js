3f19d248f675e44b2d9f5d466d520e835fc82e97
quadriviumradio <quadriviumradio@gmail.com>
2016-04-04 21:07:53 -0400

    Ajout de lieux.js

---
+++ lieux.js
@@ -0,0 +1,121 @@
+var lieux = {"portage": [ 
+      {
+      "titre": "Portage Glacier Road",
+      "media": "T03",
+    "pos": {lat: 60.788247, lng: -148.880222},
+    "lien":"portage",
+    "texte":"Non loin de Whittier, un sentier fuit malicieusement dans les coulisses du sous-bois calme, comme pour se cacher avant une certaine f&ecirc;te."
+      }],
+   "louise": [ 
+      {
+      "titre": "Louise Lake",
+      "media": "T05",
+    "pos": {lat: 62.264764, lng: -146.522107},
+    "lien":"louise",
+    "texte":"Las. Sous sa couette &eacute;paisse, endormi. Le Lac Louise assiste enfin &agrave; sa pi&egrave;ce intime. Il r&ecirc;ve d'un Soleil qui fige dans le ciel. La Nuit entre en sc&egrave;ne aussi vite qu'un arbre grimpant &agrave; la cime de la for&ecirc;t. La Nuit vient, mais le Soleil ne se pousse pas. Cet espace est &agrave; l'arr&ecirc;t. Cette lumi&egrave;re est &agrave; l'arr&ecirc;t. D'une main affectueuse, un vent maternel caresse le duvet blanc. Pr&eacute;cieux mouvement."
+    }],
+       "copper_center": [ 
+      {
+      "titre": "Copper Center",
+      "media": "T17",
+    "pos": {lat: 61.954202, lng: -145.303912},
+    "lien":"copper_center",
+    "texte":"La Nuit a r&eacute;ussi &agrave; jeter son rideau &eacute;pais sur le Soleil. Le th&eacute;&acirc;tre est ferm&eacute;, les spectateurs sont partis. Mais un d&eacute;cor tr&ocirc;ne et un projecteur est rest&eacute; allum&eacute;."
+      }],
+       "copper_river": [ 
+      {
+      "titre": "Copper River",
+      "media": "T20",
+    "pos": {lat: 62.260634, lng: -145.353442},
+    "lien": "copper_river",
+    "texte":"Les r&ecirc;ves s'&eacute;coulent tendrement... Le Soleil se voit en projecteur de th&eacute;&acirc;tre. Il invente des couleurs et illumine des petites vies insouciantes. La Nuit se baigne dans un lac enti&egrave;rement lisse et blanc. Aucune vague ne lui &eacute;clabousse les yeux, et le fond est invisible. Le Vent enlace un sentier myst&eacute;rieux. Les oiseaux rebondissent et &eacute;coutent la route au loin."
+      }],   
+       "richardson": [ 
+      {
+      "titre": "Richardson Highway",
+      "media": "T22",
+    "pos": {lat: 62.781095, lng: -145.469012},
+    "lien": "richardson",
+    "texte": "Une pelote jaune roule et s'&eacute;tire sur la route. Elle se fragmente pour aller plus loin. Les vieux arbres, curieux, la regarde. Ce qu'ils ne comprennent pas, c'est que la pelote tisse une pi&egrave;ce infinie qui s'appelle Distance, et qu'elle va bient&ocirc;t briser le mur du son."
+      }],  
+    "paxson": [ 
+      {
+      "titre": "Paxson Plateau",
+      "media": "T23",
+    "pos": {lat: 63.163225, lng: -145.530029},
+    "lien": "paxson",
+    "texte": "La Nuit int&eacute;rieure est &eacute;blouissante. Le chemin est dans la lune. Une &eacute;toile dit vague. A moins que ce soit un ours ! Ou peut-&shy;&ecirc;tre un orignal... Son cri porte le silence sur un Plateau d'argent."
+      }],  
+    "forest": [ 
+      {
+      "titre": "Forest near Steese Highway",
+      "media": "T28",
+    "pos": {lat: 65.212594, lng: -147.089364},
+    "lien": "forest",
+     "texte": "Le coeur canin. Lointain. M&eacute;lancolique. Intangible. Est parti. Les vieux rugueux, et piquants, ne s'en plaignent pas. Pour eux, nul besoin de piquer le temps. Car la tranquillit&eacute; suffit. Quoiqu'ils n'arr&ecirc;tent pas la fougue des petites f&eacute;es qui bondissent inopin&eacute;ment sur leurs bras rigides."
+      }],  
+    "dog_choir": [ 
+      {
+      "titre": "Dog choir near Steese Highway",
+      "media": "T35",
+    "pos": {lat: 65.166280, lng: -147.301179},
+    "lien": "dog_choir",
+    "texte": "La cath&eacute;drale bois&eacute;e a invit&eacute; un coeur vigoureux. Le chant bat son plein au moment de l'entracte."
+      }], 
+    "carrier": [ 
+      {
+      "titre": "Carrier near Ellioth Highway",
+      "media": "T36",
+    "pos": {lat: 65.245216, lng: -148.141850},
+    "lien": "carrier",
+    "texte": "Min&eacute;ralit&eacute; inqui&eacute;tante. Le poids de l'air roule sur les cailloux..."
+      }],     
+    "minto": [ 
+ {
+      "titre": "Minto Village", 
+       "media": "T40",
+    "pos": {lat: 65.153523, lng: -149.335820},
+    "lien": "minto",
+    "texte":"Le bout du monde est froid, mais pas d&eacute;nu&eacute; des sens."
+      }],  
+    "ellioth": [ 
+ {
+      "titre": "Ellioth Highway", 
+       "media": "T46",
+    "pos": {lat: 65.299243, lng: -149.208992},
+    "lien": "ellioth",
+    "texte":"Vivacit&eacute; des fines herbes, engag&eacute;es dans la danse froide d'un appel infini... qui approche..."
+      }],  
+    "borealis": [ 
+ {
+      "titre": "Northern Light", 
+       "media": "T49",
+    "pos": {lat: 65.366755, lng: -148.955537},
+    "lien": "borealis",
+    "texte":"Apparition farouche sur la sc&egrave;ne nocturne. Danse c&eacute;leste. Vedette furtive et silencieuse.<br />Clou du spectacle. Les fines herbes, jalouses par cette chor&eacute;graphie mystique, s'inclinent tout de m&ecirc;me devant tant de talent. Des visages pi&eacute;g&eacute;s dans la nuit grincent et grimacent avec leurs longs doigts secs. Tout le monde s'y attendait, mais tout le monde est surpris.<br />La grande voile p&acirc;le flotte au vent."
+      }],  
+    "park_road": [ 
+{
+      "titre": "Denali Park Road", 
+       "media": "T53",
+    "pos": {lat: 63.716166, lng: -149.019712},
+    "lien": "park_road",
+    "texte":"Une baleine blanche roule sa bosse. Le fr&ecirc;le protagoniste, de ses multiples bras pleins de doigts, nargue son adversaire. Pourtant, les tambours m&eacute;lodramatiques agitent les fines herbes d'un frisson inqui&eacute;tant."
+      }],
+    "denali": [ 
+{
+      "titre": "Denali", 
+       "media": "T54",
+    "pos": {lat: 63.740590, lng: -149.291352},
+    "lien": "denali",
+    "texte":"Le Soleil est en r&eacute;gie. Depuis sa place favorite, il inonde maintenant la sc&egrave;ne d'un feu &eacute;clatant. Le jeune premier souffle son texte avec conviction, au sein d'un d&eacute;cor chaotique."
+      }],
+    "savage_river": [ 
+  {
+      "titre": "Savage River", 
+       "media": "T55",
+    "pos": {lat: 63.774522, lng: -149.306080},
+    "lien":"savage_river",
+    "texte":"Les r&ecirc;ves s'acheminent vers un nouveau spectacle. Le souvenir d'une lumi&egrave;re ensorcelante hante encore les vaguelettes fr&eacute;tillantes."
+      }]    
+}
